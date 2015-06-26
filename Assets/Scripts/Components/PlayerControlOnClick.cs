0b0dfe78b64f2196d4634ec76614832b40c26fa1
dendimaniac <daq30899@hotmail.com>
2015-06-26 15:45:25 +0700

    Separate mouse control and remove the ability to jump

---
+++ Assets/Scripts/Components/PlayerControlOnClick.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControlOnClick : MonoBehaviour {
+
+	public GameObject inventory;
+	bool isMoving = false;
+	public float moveSpeed;
+	[HideInInspector]
+	public Vector3 mousePosition;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0))
+		{
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			StartCoroutine(MovePlayer(mousePosition));
+		}
+	}
+
+	IEnumerator MovePlayer (Vector3 endPosition)
+	{
+		Vector3 targetPosition = new Vector3 (endPosition.x, gameObject.transform.position.y, gameObject.transform.position.z);
+		while (isMoving == false)
+		{
+			while (Vector3.Distance(gameObject.transform.position, targetPosition) > 0.05f)
+			{
+				isMoving = true;
+				gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, targetPosition, Time.deltaTime * moveSpeed);
+				yield return null;
+			}
+		}
+		isMoving = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D item)
+	{
+		inventory.GetComponent<Inventory> ().AddToInventory (item);
+		Destroy (item.gameObject);
+	}
+}
---
+++ Assets/Scripts/Components/SwitchPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPlayer : MonoBehaviour {
+
+	public GameObject topPlayer;
+	public GameObject botPlayer;
+
+	void Start ()
+	{
+		topPlayer.GetComponent<PlayerControlOnClick> ().enabled = true;
+		botPlayer.GetComponent<PlayerControlOnClick> ().enabled = false;
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (1))
+		{
+			if (topPlayer.GetComponent<PlayerControlOnClick> ().enabled == true)
+			{
+				topPlayer.GetComponent<PlayerControlOnClick> ().enabled = false;
+				botPlayer.GetComponent<PlayerControlOnClick> ().enabled = true;
+			}
+			else
+			{
+				topPlayer.GetComponent<PlayerControlOnClick> ().enabled = true;
+				botPlayer.GetComponent<PlayerControlOnClick> ().enabled = false;
+			}
+		}
+	}
+}
